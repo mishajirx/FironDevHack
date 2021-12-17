6c13da7a3fed243b341f2c2ca109e6a5873b2ec5
Mishazher <mishalolhopengok@gmail.com>
2021-12-18 00:11:38 +0300

    Retested on pythonanywhere. All is fine

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # FironDevHack
 It's a project for a case from "Firon" company
+
+## Starting the project
+
+1. Run wsgi.py file
--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-from flask import Flask, make_response, jsonify, abort, request, blueprints
+from flask import Flask, make_response, jsonify, abort, request, blueprints, render_template
 from waitress import serve
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-x = 0
 
 
 @app.route('/')
 def hello():
-    return 'Hello world'
+    return render_template('base.html')
 
 
 def main():
