24f76dafabb8e2c709a6c8549a7c732bda1da48b
Mishazher <mishalolhopengok@gmail.com>
2021-12-19 13:48:47 +0300

    Added Ability to add site

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from flask import Flask, make_response, jsonify, abort, request, blueprints, render_template
+from flask import Flask, make_response, jsonify, abort, request, blueprints, render_template, redirect
+from flask_wtf import FlaskForm
 from waitress import serve
-from json import load
+from json import loads, dumps,load
 from requests import get
+import psycopg2
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -16,6 +20,23 @@ ans = {
 }
 
 
+class LoginForm(FlaskForm):
+    address = StringField('Полный адрес сайта', validators=[DataRequired()])
+    submit = SubmitField('Добавить')
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    form = LoginForm()
+    if form.validate_on_submit():
+        sites['sites'].append({"address": form.address.data, "state": "-"})
+        print(sites)
+        with open("sites.json", "w") as f:
+            f.write(dumps(sites))
+        return redirect('/')
+    return render_template('newsite.html', title='Добавление IP', form=form)
+
+
 @app.route('/')
 def hello():
     for i in range(len(sites['sites'])):
@@ -25,7 +46,7 @@ def hello():
             sites['sites'][i]['state'] = ans[res.status_code]
         except Exception:
             sites['sites'][i]['state'] = "Nonactive"
-    return render_template('base.html', sites=sites)
+    return render_template('main.html', sites=sites)
 
 
 def main():
