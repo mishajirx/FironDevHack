9c1acaebf9470a50fb5c9abaf774df285228fc4d
Mishazher <mishalolhopengok@gmail.com>
2021-12-19 06:16:28 +0300

    Added not-working IP-Addresses table

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from flask import Flask, make_response, jsonify, abort, request, blueprints, render_template
 from waitress import serve
+from json import load
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
+sites = load(open("sites.json"))
+
 @app.route('/')
 def hello():
-    return render_template('base.html')
+    return render_template('base.html', sites=sites)
 
 
 def main():
